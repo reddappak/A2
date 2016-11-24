c314d8ae066e90d97f55ee30e90c161173b0c89e
reddappak <reddappak@yahoo.com>
2016-11-24 13:24:50 +0530

    v1.0

---
+++ src/test/java/a2Package/A2Class.java
@@ -0,0 +1,11 @@
+package a2Package;
+
+public class A2Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello from A2class");
+
+	}
+
+}
